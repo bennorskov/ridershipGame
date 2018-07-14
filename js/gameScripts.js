343826b79d5d762795b2a40bff27def43ef5bbd5
Ben Norskov <norskovb@newschool.edu>
2018-07-14 01:06:57 -0400

    Halfway through adding a station to the bottom

--- js/gameScripts.js
+++ js/gameScripts.js
@@ -1,7 +1,5 @@
-var displayCard;
 var timeout = null;
 var gameModule = (function () {
-	var currentStation = null;
 	var dataSet = {
 		"14th St-Union Square": {
 			lines: ["4", "5", "6", "n", "q", "r", "l"],
@@ -56,8 +54,7 @@ var gameModule = (function () {
 			ridership: 17095073
 		}
 	};
-	function addLine( lineName ) {
-		var _html = '';
+	function returnClassNameFromLineName(lineName) {
 		var className;
 		switch(lineName) {
 			case "4":
@@ -98,28 +95,60 @@ var gameModule = (function () {
 				className = lineName;
 				break;
 		}
+		return className;
+	};
+	function addLineToDisplayCard( lineName ) {
+		var _html = '';
+		var className = returnClassNameFromLineName(lineName);
 		_html += "<div class='displayCard__lines--circle ";
 		_html += className + "'>" + lineName.toUpperCase() + "</div>";
 		return _html;
 	}
 	function checkSwipeVsSelected(swipedStation, direction) {
-		console.log(swipedStation);
-		currentStation = document.getElementsByClassName("selectedStation")[0];
-		if (currentStation === null) {
-			addToBottomScroll();
+		// console.log(swipedStation);
+		// temp test code:
+		gameModule.currentStation = document.getElementsByClassName("selectedStation")[0];
+		console.log(gameModule.currentStation);
+		// end test code
+		if (gameModule.currentStation === null) {
+			addToBottomScroll(swipedStation);
 			return;
 		}
 		current = dataSet[swipedStation].ridership;
-		console.log(currentStation.getElementsByClassName("stationOrder__station--label")[0].innerText );
-		selected = dataSet[ currentStation.getElementsByClassName("stationOrder__station--label")[0].innerText ].ridership;
-		if (current > selected && direction > 0) {
+		console.log(gameModule.currentStation.getElementsByClassName("stationOrder__station--label")[0].innerText );
+		selected = dataSet[ gameModule.currentStation.getElementsByClassName("stationOrder__station--label")[0].innerText ].ridership;
+		if (current > selected && direction > 0) { // bigger swipe (right)
 			addToBottomScroll(swipedStation);
-		} else if (current < selected && direction < 0){
+		} else if (current < selected && direction < 0){ // smaller swipe (left)
 			addToBottomScroll(swipedStation);
 		}
+		changeDisplayCard("Lexington Av-53 St");
 	}
-	function addToBottomScroll( elementToAdd ) {
-		console.log(elementToAdd + " Added to bottom!");
+	function addToBottomScroll( stationToAdd ) {
+		console.log(stationToAdd + " Added to bottom!");
+		var statOrd = document.getElementsByClassName("stationOrder")[0];
+		var lineName = dataSet[stationToAdd].lines[ Math.floor(dataSet[stationToAdd].lines.length * Math.random()) ];
+		var className = returnClassNameFromLineName(lineName);
+		var _html = "<div class='stationOrder__station selectedStation " + className + "'>";
+		_html += "<h1 class='stationOrder__station--label'>" + stationToAdd + "</h1>";
+		_html += "<h1 class='stationOrder__station--lineLabel'>"+ lineName.toUpperCase() +"</h1>";
+		_html += "</div>";
+		var temp = document.createElement("template");
+		temp.innerHTML = _html.trim();
+		console.log("Temp is " + temp.innerHTML);
+		statOrd.appendChild(temp.content.firstChild);
+		gameModule.currentStation = temp;
+	}
+	function changeDisplayCard(stationName) {
+		console.log("added " + stationName + " card")
+		var _html = "<hr><h1 class='displayCard__title'>" + stationName + "</h1>";
+		_html += "<div class='displayCard__lines'>";
+		var lines = dataSet[stationName].lines;
+		for (var i = lines.length - 1; i >= 0; i--) {
+			_html += addLineToDisplayCard(lines[i]) + " ";
+		};
+		_html += "</div>";
+		gameModule.displayCard.innerHTML = _html;
 	}
 
 	// ——————— ——————— public methods below
@@ -127,32 +156,33 @@ var gameModule = (function () {
 		getDataSet: function() {
 			return dataSet;
 		},
-		addLines: function() {},
+		displayCard: "",
+		currentStation: null,
 		lockBottomScroll: function (buttonToLock) {},
 		checkSwipeAgainstSelected: function () {},
-		init: function () {
-			displayCard = document.getElementsByClassName("displayCard")[0];
-			Transform(displayCard);
-			new AlloyFinger(displayCard, {
+		setupSwipeEvent: function () {
+			// set up swipe event
+			Transform(this.displayCard);
+			new AlloyFinger(this.displayCard, {
 			    pressMove:function(evt){
 			    	if (timeout != null) clearInterval(timeout);
 			    	var maxMove = 100;
 			    	// var limit = Math.min(evt.deltaX
-			    	if ( Math.abs(displayCard.translateX + evt.deltaX) <= maxMove) {
-			        	displayCard.translateX += evt.deltaX;
+			    	if ( Math.abs(gameModule.displayCard.translateX + evt.deltaX) <= maxMove) {
+			        	gameModule.displayCard.translateX += evt.deltaX;
 			        } else {
-			        	if (displayCard.translateX < 0) displayCard.translateX = -maxMove;
-			        	if (displayCard.translateX > 0) displayCard.translateX = maxMove;
+			        	if (gameModule.displayCard.translateX < 0) gameModule.displayCard.translateX = -maxMove;
+			        	if (gameModule.displayCard.translateX > 0) gameModule.displayCard.translateX = maxMove;
 			        }
 			        evt.preventDefault();
 
-			    	var percentageOfMove = displayCard.translateX/maxMove;
+			    	var percentageOfMove = gameModule.displayCard.translateX/maxMove;
 			    	var xRotationMax = 10,
 			    		yRotationMax = 20,
 			    		zRotationMax = 10;
-			    	displayCard.rotateX = Math.abs(percentageOfMove) * xRotationMax;
-			    	displayCard.rotateY = percentageOfMove * yRotationMax;
-			    	displayCard.rotateZ = percentageOfMove * zRotationMax;
+			    	gameModule.displayCard.rotateX = Math.abs(percentageOfMove) * xRotationMax;
+			    	gameModule.displayCard.rotateY = percentageOfMove * yRotationMax;
+			    	gameModule.displayCard.rotateZ = percentageOfMove * zRotationMax;
 			    },
 			    // Either: Check to see if the card should add, 
 			    // OR: Put the card back into place
@@ -160,34 +190,39 @@ var gameModule = (function () {
 			    	var maxMove = 100;
 			    	if (timeout != null) clearInterval(timeout);
 			    	var completePercentage = .85;
-			    	var percentageOfMove = displayCard.translateX/maxMove;
-			    	if (displayCard.translateX/maxMove > completePercentage) {
-			    		checkSwipeVsSelected( displayCard.getElementsByClassName("displayCard__title")[0].innerText, 1 );
-			    	} else if (displayCard.translateX/maxMove < -completePercentage) {
-			    		checkSwipeVsSelected( displayCard.getElementsByClassName("displayCard__title")[0].innerText, -1 );
-			    	} else {
-			    		timeout = setInterval(function () {
-					    	var xRotationMax = 10,
-					    		yRotationMax = 20,
-					    		zRotationMax = 10;
-					    	var easeAmount = .55;
-					    	displayCard.translateX += (0 - displayCard.translateX) * easeAmount;
-					    	percentageOfMove = displayCard.translateX/maxMove; // calculate after movement for rotation
-					    	displayCard.rotateX = Math.abs(percentageOfMove) * xRotationMax;
-					    	displayCard.rotateY = percentageOfMove * yRotationMax;
-					    	displayCard.rotateZ = percentageOfMove * zRotationMax;
-					    	if (Math.abs(percentageOfMove) < .05) {
-					    		console.log("interval cleared!");
-					    		clearInterval(timeout);
-						    	displayCard.translateX = 0;
-						    	displayCard.rotateX = 0;
-						    	displayCard.rotateY = 0;
-						    	displayCard.rotateZ = 0;
-					    	}
-			    		}, 50);
-				    }
+			    	var percentageOfMove = gameModule.displayCard.translateX/maxMove;
+			    	if (gameModule.displayCard.translateX/maxMove > completePercentage) {
+			    		checkSwipeVsSelected( gameModule.displayCard.getElementsByClassName("displayCard__title")[0].innerText, 1 );
+			    		//gameModule.displayCard.parentNode.removeChild(gameModule.displayCard);
+			    	} else if (gameModule.displayCard.translateX/maxMove < -completePercentage) {
+			    		checkSwipeVsSelected( gameModule.displayCard.getElementsByClassName("displayCard__title")[0].innerText, -1 );
+			    		//gameModule.displayCard.parentNode.removeChild(gameModule.displayCard);
+			    	}
+		    		timeout = setInterval(function () {
+				    	var xRotationMax = 10,
+				    		yRotationMax = 20,
+				    		zRotationMax = 10;
+				    	var easeAmount = .55;
+				    	gameModule.displayCard.translateX += (0 - gameModule.displayCard.translateX) * easeAmount;
+				    	percentageOfMove = gameModule.displayCard.translateX/maxMove; // calculate after movement for rotation
+				    	gameModule.displayCard.rotateX = Math.abs(percentageOfMove) * xRotationMax;
+				    	gameModule.displayCard.rotateY = percentageOfMove * yRotationMax;
+				    	gameModule.displayCard.rotateZ = percentageOfMove * zRotationMax;
+				    	if (Math.abs(percentageOfMove) < .05) {
+				    		// console.log("interval cleared!");
+				    		clearInterval(timeout);
+					    	gameModule.displayCard.translateX = 0;
+					    	gameModule.displayCard.rotateX = 0;
+					    	gameModule.displayCard.rotateY = 0;
+					    	gameModule.displayCard.rotateZ = 0;
+				    	}
+		    		}, 50);
 			    }
 			});
+		},
+		init: function () {
+			this.displayCard = document.getElementsByClassName("displayCard")[0];
+			gameModule.setupSwipeEvent();
 		}
 	}
 })();

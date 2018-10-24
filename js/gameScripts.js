46caa303931191c4eb14acd2ee454f7fd117ab1b
Will Scott <wtscott@gmail.com>
2018-10-24 14:22:34 +0200

    Created moveBottomScroll function to prevent redundant code in pressMove and tap events.
    Also, cleaned up code from testing

--- js/gameScripts.js
+++ js/gameScripts.js
@@ -264,6 +264,27 @@ console.log("chiRiders: " + chiRiders);
         dataSet.delete(stationToAdd);  
         lockBottomScroll(gameModule.selectedStation);
 	}
+    function moveBottomScroll(deltaX) {
+        // Move stationOrd element along x axis
+        var screenMiddle = window.screen.width * .5;
+        var curLeft = gameModule.stationOrd.offsetLeft;
+        var newPosition = deltaX + curLeft;
+        
+        // If there is a station selected, unselect it while scrolling
+        var selected = document.getElementsByClassName("selectedStation")[0] || null;
+        if (selected != null) {
+            selected.classList.remove("selectedStation");
+            document.getElementsByClassName("stationOrder__selectedStationLabel")[0].innerText = "";
+        }
+        // Move stationOrd, restricting it from leaving the screen entirely
+        if(newPosition > screenMiddle) {
+            newPosition = screenMiddle;
+        }
+        else if(newPosition < screenMiddle - gameModule.stationOrd.offsetWidth) {
+            newPosition = screenMiddle - gameModule.stationOrd.offsetWidth;
+        }
+        gameModule.stationOrd.style.left = newPosition + "px";
+    }
 	function lockBottomScroll ( stationToLock ) {
 		// stationToLock is a dom element or null
 		// eventually we should click on a station and it'll scroll to it
@@ -359,53 +380,20 @@ console.log("chiRiders: " + chiRiders);
 		selectedStation: null,
 		setupSwipeEvent: function () {
 			// ————— ————— ————— ————— set up swipe event for bottom container
-//			Transform(this.bottomScrollContainer);
 			new AlloyFinger(this.bottomScrollContainer, {
                 touchStart: function(evt) { 
-console.log("touchStart!");  
-console.log("evt: "  + evt.touches[0].pageX);
                     // Get x location of finger press to use in tap events
                     gameModule.bottomScrollContainer.touchStartX = evt.touches[0].pageX;
-                    gameModule.bottomScrollContainer.translateX = 0;
                     evt.preventDefault();
                 },
-				pressMove: function(evt) {
-console.log("pressMove!");
-                    var screenMiddle = window.screen.width * .5;
-                    var curLeft = gameModule.stationOrd.offsetLeft;
-                    var newPosition = evt.deltaX + curLeft;
-                    gameModule.bottomScrollContainer.translateX += Math.abs(evt.deltaX);
-                    evt.preventDefault();
-
-                    // If there is a station selected, unselect it while scrolling
-					var selected = document.getElementsByClassName("selectedStation")[0] || null;
-                    if (selected != null) {
-						selected.classList.remove("selectedStation");
-                        document.getElementsByClassName("stationOrder__selectedStationLabel")[0].innerText = "";
-					}
-                    // Move stations, restricting them from leaving the screen entirely
-                    if(newPosition > screenMiddle) {
-                        newPosition = screenMiddle;
-                    }
-                    else if(newPosition < screenMiddle - gameModule.stationOrd.offsetWidth) {
-                        newPosition = screenMiddle - gameModule.stationOrd.offsetWidth;
-                    }
-                    gameModule.stationOrd.style.left = newPosition + "px";
+				pressMove: function(evt) {                    
+                    moveBottomScroll(evt.deltaX);
 				}, 
 				touchEnd: function() {
-console.log("End!");
-                    // Check for tap
-                    if(gameModule.bottomScrollContainer.translateX == 0) {
-                        
-                    }
                     lockBottomScroll();
 				},
-                // This function could happen inside touchEnd, which may be more efficient....
                 tap: function() {
-console.log("Tap!");
                     var screenWidth = window.screen.width;
-                    var screenMiddle = screenWidth * .5;
-                    var curLeft = gameModule.stationOrd.offsetLeft;
                     var delta = 0;
                     
                     if (gameModule.bottomScrollContainer.touchStartX < screenWidth * .34) {
@@ -415,26 +403,7 @@ console.log("Tap!");
                         delta = screenWidth * -.25;
                     }
                     
-                    var newPosition = curLeft + delta;
-console.log("curLeft: "  + curLeft);  
-console.log("change: "  + delta);  
-console.log("newPosition: "  + newPosition);
-                    // If there is a station selected, unselect it
-					var selected = document.getElementsByClassName("selectedStation")[0] || null;
-                    if (selected != null) {
-						selected.classList.remove("selectedStation");
-                        document.getElementsByClassName("stationOrder__selectedStationLabel")[0].innerText = "";
-					}
-                    // Move stations, restricting them from leaving the screen entirely
-                    if(newPosition > screenMiddle) {
-                        newPosition = screenMiddle;
-                    }
-                    else if(newPosition < screenMiddle - gameModule.stationOrd.offsetWidth) {
-                        newPosition = screenMiddle - gameModule.stationOrd.offsetWidth;
-                    }
-console.log("final: "  + newPosition);
-                    gameModule.stationOrd.style.left = newPosition + "px";
-                    
+                    moveBottomScroll(delta);               
                     lockBottomScroll();
                 }
 			});
